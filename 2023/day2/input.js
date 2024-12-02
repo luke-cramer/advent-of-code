ba8d9f74c9a7e471788e52bdfdd9d6431fa4a277
luke-cramer <luke.cramer@amenitieshealth.com>
2024-12-01 23:19:12 -0800

    Add 2023

---
+++ 2023/day2/input.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+const currentDir = path.dirname(new URL(import.meta.url).pathname);
+const inputPath = path.join(currentDir, 'input.txt');
+const examplePath = path.join(currentDir, 'example.txt');
+
+let example = [];
+let input = [];
+
+try {
+    const exampleData = fs.readFileSync(examplePath, 'utf8');
+    const data = fs.readFileSync(inputPath, 'utf8');
+
+    example = exampleData.split(/\r?\n/);
+    input = data.split(/\r?\n/);
+} catch (err) {
+    console.error('Error reading input', err);
+}
+
+export const day2example = example;
+export const day2input = input;
--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-Advent of Code
-- 2023
-- 2024
+To create a folder for a year Y and day N:
+```bash
+npm run create -- Y D
+```
+
+To start run `dayN/script.js` on save:
+```bash
+npm run watch -- Y D
+```
+
+To init rust
+```
+cargo init --bin
+cargo install cargo-watch
+```
+
+To watch with rust:
+```bash
+cargo watch -x run -c
+```
